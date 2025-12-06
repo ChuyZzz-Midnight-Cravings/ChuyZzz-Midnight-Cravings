5918e870a08a7d9d13964eb4bc91de98a1e89634
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-12-06 09:33:11 +0800

    owrayt chuy kaau gwapo ko jum2

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,34 +1,36 @@
 const PRODUCTS = [
   // CHIPS
-  {id:'c1',name:'Midnight Caramel Crumble',price:145,img:'assets/images/chips/caramel-cramble.png', category:'chips'},
-  {id:'c2',name:'Moonight Cookie Essence',price:155,img:'assets/images/chips/cookie-essence.png', category:'chips'},
-  {id:'c3',name:'Dark Nebula Cocoa',price:150,img:'assets/images/chips/nebula-cocoa.png', category:'chips'},
-  {id:'c4',name:'Lunar Choco Drizzle',price:155,img:'assets/images/chips/chooco-drizzle.png', category:'chips'},
-  {id:'c5',name:'Ruffles Flamin Hot',price:35,img:'assets/images/chips/ruffles.png', category:'chips'},
-  {id:'c6',name:'Lays Classic',price:85,img:'assets/images/chips/lays.png', category:'chips'},
-  {id:'c7',name:'Piattos Cheese',price:40,img:'assets/images/chips/piattos-cheese.png', category:'chips'},
-  {id:'c8',name:'Piattos Nacho Pizza',price:45,img:'assets/images/chips/piattos-pizza.png', category:'chips'},
-  {id:'c9',name:'Piattos Sour Cream & Onion',price:45,img:'assets/images/chips/piattos-sco.png', category:'chips'},
+  {id:'c1', name:'Midnight Caramel Crumble', price:120, img:'assets/images/chips/caramel-cramble.png', category:'chips'},
+  {id:'c2', name:'Moonight Cookie Essence', price:125, img:'assets/images/chips/cookie-essence.png', category:'chips'},
+  {id:'c3', name:'Dark Nebula Cocoa', price:120, img:'assets/images/chips/nebula-cocoa.png', category:'chips'},
+  {id:'c4', name:'Lunar Choco Drizzle', price:125, img:'assets/images/chips/chooco-drizzle.png', category:'chips'},
+  {id:'c5', name:'Ruffles Flamin Hot', price:45, img:'assets/images/chips/ruffles.png', category:'chips'},
+  {id:'c6', name:'Lays Classic', price:60, img:'assets/images/chips/lays.png', category:'chips'},
+  {id:'c7', name:'Piattos Cheese', price:45, img:'assets/images/chips/piattos-cheese.png', category:'chips'},
+  {id:'c8', name:'Piattos Nacho Pizza', price:50, img:'assets/images/chips/piattos-pizza.png', category:'chips'},
+  {id:'c9', name:'Piattos Sour Cream & Onion', price:50, img:'assets/images/chips/piattos-sco.png', category:'chips'},
+  {id:'c10', name:'Oishi', price:45, img:'assets/images/chips/oi.webp', category:'chips'},
 
   // DRINKS
-  {id:'d1',name:'Coke-in-a-Can',price:45,img:'assets/images/drinks/coke.png', category:'drinks'},
-  {id:'d2',name:'Sprite-in-a-Can',price:45,img:'assets/images/drinks/sprite.png', category:'drinks'},
-  {id:'d3',name:'Royal-in-a-Can',price:88,img:'assets/images/drinks/royal.png', category:'drinks'},
-  {id:'d4',name:'Grape Royal',price:85,img:'assets/images/drinks/royal-grape.png', category:'drinks'},
-  {id:'d5',name:'Pepsi-in-a-Can',price:85,img:'assets/images/drinks/pepsi.png', category:'drinks'},
+  {id:'d1', name:'Coke-in-a-Can', price:50, img:'assets/images/drinks/coke.png', category:'drinks'},
+  {id:'d2', name:'Sprite-in-a-Can', price:50, img:'assets/images/drinks/sprite.png', category:'drinks'},
+  {id:'d3', name:'Royal-in-a-Can', price:55, img:'assets/images/drinks/royal.png', category:'drinks'},
+  {id:'d4', name:'Grape Royal', price:55, img:'assets/images/drinks/royal-grape.png', category:'drinks'},
+  {id:'d5', name:'Pepsi-in-a-Can', price:50, img:'assets/images/drinks/pepsi.png', category:'drinks'},
 
   // FOODS
-  {id:'f1', name:'Burger', price:120, img:'assets/images/Foods/buerger.jpg', category:'foods'},
-  {id:'f2', name:'Fried Chicken', price:150, img:'assets/images/Foods/chekin joy.jpg', category:'foods'},
+  {id:'f1', name:'Burger', price:130, img:'assets/images/Foods/buerger.jpg', category:'foods'},
+  {id:'f2', name:'Fried Chicken', price:160, img:'assets/images/Foods/chekin joy.jpg', category:'foods'},
   {id:'f3', name:'Fries', price:70, img:'assets/images/Foods/fries.jpg', category:'foods'},
-  {id:'f4', name:'Halo Halo', price:60, img:'assets/images/Foods/halo2.jpg', category:'foods'},
+  {id:'f4', name:'Halo Halo', price:65, img:'assets/images/Foods/halo2.jpg', category:'foods'},
   {id:'f5', name:'Pizza Cheese', price:180, img:'assets/images/Foods/pitza cheez.jpg', category:'foods'},
-  {id:'f6', name:'Spaghetti', price:130, img:'assets/images/Foods/spag.jpg', category:'foods'}, 
-  {id:'f7', name:'Shawarma', price:75, img:'assets/images/Foods/sawarma.jpg', category:'foods'},    
-  {id:'f8', name:'Siomai', price:130, img:'assets/images/Foods/soimai.jpg', category:'foods'},
-  {id:'f9', name:'Siopao', price:130, img:'assets/images/Foods/sopaw.jpg', category:'foods'},      
-  {id:'f10', name:'Mango Float', price:130, img:'assets/images/Foods/mango.webp', category:'foods'}
+  {id:'f6', name:'Spaghetti', price:135, img:'assets/images/Foods/spag.jpg', category:'foods'}, 
+  {id:'f7', name:'Shawarma', price:80, img:'assets/images/Foods/sawarma.jpg', category:'foods'},    
+  {id:'f8', name:'Siomai', price:50, img:'assets/images/Foods/soimai.jpg', category:'foods'},
+  {id:'f9', name:'Siopao', price:50, img:'assets/images/Foods/sopaw.jpg', category:'foods'},      
+  {id:'f10', name:'Mango Float', price:90, img:'assets/images/Foods/mango.webp', category:'foods'}
 ];
+
 
 function renderShop(){
   const chipsContainer = document.getElementById('chips-list');
