a74d2fe188d3a31d596b0154a90aa494db038bc3
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-12-06 09:07:59 +0800

    added halo2

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -21,6 +21,7 @@ const PRODUCTS = [
   {id:'f1',name:'Burger',price:45,img:'assets/images/Foods/buerger.jpg', category:'foods'},
   {id:'f2',name:'Fried Chicken',price:85,img:'assets/images/Foods/chekin joy.jpg', category:'foods'},
   {id:'f3',name:'Fries',price:55,img:'assets/images/Foods/fries.jpg', category:'foods'},
+  {id:'f4',name:'halo2',price:35,img:'assets/images/Foods/halo2.jpg', category:'foods'},
   
 ];
 
